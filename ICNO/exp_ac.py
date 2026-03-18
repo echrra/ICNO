e3624a1e4523b754c566ff288ae91995698616c7
echrra <50135358+echrra@users.noreply.github.com>
2026-03-18 17:28:49 +0800

    Add files via upload

---
+++ ICNO/exp_ac.py
@@ -0,0 +1,255 @@
+
+
+import os
+import argparse
+import numpy as np
+import scipy.io as scio
+import time
+
+parser = argparse.ArgumentParser('Training Transolver')
+
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--epochs', type=int, default=500)
+parser.add_argument('--weight_decay', type=float, default=1e-5)
+parser.add_argument('--model', type=str, default='Transolver_Structured_Mesh_2D')
+parser.add_argument('--n-hidden', type=int, default=64, help='hidden dim') #128
+parser.add_argument('--n-layers', type=int, default=8, help='layers')
+parser.add_argument('--batch-size', type=int, default=8)#4
+parser.add_argument("--gpu", type=str, default='7', help="GPU index to use")
+parser.add_argument('--max_grad_norm', type=float, default=0.1)
+parser.add_argument('--downsample', type=int, default=1)
+parser.add_argument('--mlp_ratio', type=int, default=1)
+parser.add_argument('--dropout', type=float, default=0.0)
+parser.add_argument('--ntrain', type=int, default=1000)
+parser.add_argument('--gate', type=float, default=0)
+parser.add_argument('--eval', type=int, default=0)
+parser.add_argument('--save_name', type=str, default='ICNO_ac')
+parser.add_argument('--data_path', type=str, default='/DATA')
+args = parser.parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+
+loss_log_file = "./checkpoints/{}_loss.txt".format(args.save_name)
+os.makedirs(os.path.dirname(loss_log_file), exist_ok=True)  
+
+import torch
+import torch.nn.functional as F
+from tqdm import *
+from utils.testloss import TestLoss
+from einops import rearrange
+from model_dict import get_model
+from utils.normalizer import UnitTransformer
+import matplotlib.pyplot as plt
+
+epochs = 500
+eval = args.eval
+save_name = args.save_name
+
+
+def count_parameters(model):
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad: continue
+        params = parameter.numel()
+        total_params += params
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+
+
+def main():
+    ntrain = 1000
+    ntest = 100
+    N = 1100
+
+    train_path = args.data_path + '/Allen_cahn_2d_128_128_T10.mat'
+    r = args.downsample
+    h = int(((129 - 1)/r) + 1)
+    s = h
+    print(h)
+    train_data = scio.loadmat(train_path)
+    x_train = train_data['coeff'][:ntrain, ::r, ::r][:, :s, :s]
+    x_train = x_train.reshape(ntrain, -1)
+    x_train = torch.from_numpy(x_train).float()
+    y_train = train_data['sol'][:ntrain, ::r, ::r][:, :s, :s]
+    y_train = y_train.reshape(ntrain, -1)
+    y_train = torch.from_numpy(y_train)
+
+
+    test_data = scio.loadmat(train_path)
+    x_test = test_data['coeff'][-ntest:, ::r, ::r][:, :s, :s]
+    x_test = x_test.reshape(ntest, -1)
+    x_test = torch.from_numpy(x_test).float()
+    y_test = test_data['sol'][-ntest:, ::r, ::r][:, :s, :s]
+    y_test = y_test.reshape(ntest, -1)
+    y_test = torch.from_numpy(y_test)
+
+
+    x_normalizer = UnitTransformer(x_train)
+    y_normalizer = UnitTransformer(y_train)
+
+    x_train = x_normalizer.encode(x_train)
+    x_test = x_normalizer.encode(x_test)
+    y_train = y_normalizer.encode(y_train)
+
+    x_normalizer.cuda()
+    y_normalizer.cuda()
+
+    x = np.linspace(0, 1, s)
+    y = np.linspace(0, 1, s)
+    x, y = np.meshgrid(x, y)
+    pos = np.c_[x.ravel(), y.ravel()]
+    pos = torch.tensor(pos, dtype=torch.float).unsqueeze(0)
+
+    pos_train = pos.repeat(ntrain, 1, 1)
+    pos_test = pos.repeat(ntest, 1, 1)
+    print("Dataloading is over.")
+
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(pos_train, x_train, y_train),
+                                               batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(pos_test, x_test, y_test),
+                                              batch_size=args.batch_size, shuffle=False)
+
+    model = get_model(args).Model(space_dim=2,
+                                  n_layers=args.n_layers,
+                                  n_hidden=args.n_hidden,
+                                  dropout=args.dropout,
+                                  Time_Input=False,
+                                  mlp_ratio=args.mlp_ratio,
+                                  fun_dim=1,
+                                  out_dim=1,
+                                  gate = args.gate,
+                                  H=s, W=s,).cuda()
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+    print(args)
+    # print(model)
+    count_parameters(model)
+    with open(loss_log_file, "a") as f:  
+        f.write("parameters{}\n".format(count_parameters(model))) 
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, epochs=args.epochs,
+                                                    steps_per_epoch=len(train_loader))
+    myloss = TestLoss(size_average=False)
+
+    if eval:
+        model.load_state_dict(torch.load("./checkpoints/" + save_name + ".pt"), strict=False)
+        torch.save(model.state_dict(), os.path.join('./checkpoints', save_name + '_resave' + '.pt'))
+        model.eval()
+        if not os.path.exists('./results/' + save_name + '/'):
+            os.makedirs('./results/' + save_name + '/')
+
+        rel_err = 0.0
+        showcase = 10
+        id = 0
+
+
+        with torch.no_grad():
+            for pos, fx, y in test_loader:
+                id += 1
+                x, fx, y = pos.cuda(), fx.cuda(), y.cuda()
+                out = model(x, fx=fx.unsqueeze(-1)).squeeze(-1)
+                out = y_normalizer.decode(out)
+
+                tl = myloss(out, y).item()
+                rel_err += tl
+
+                # if id < showcase:
+                    # print(id)
+                    # plt.figure()
+                    # plt.axis('off')
+                    # plt.imshow(out[0, :].reshape(s, s).detach().cpu().numpy(), cmap='coolwarm')
+                    # plt.colorbar()
+                    # plt.savefig(
+                    #     os.path.join('./results/' + save_name + '/',
+                    #                 "case_" + str(id) + "_pred.pdf"))
+                    # plt.close()
+                    # # ============ #
+                    # plt.figure()
+                    # plt.axis('off')
+                    # plt.imshow(y[0, :].reshape(s, s).detach().cpu().numpy(), cmap='coolwarm')
+                    # plt.colorbar()
+                    # plt.savefig(
+                    #     os.path.join('./results/' + save_name + '/', "case_" + str(id) + "_gt.pdf"))
+                    # plt.close()
+                    # # ============ #
+                    # plt.figure()
+                    # plt.axis('off')
+                    # plt.imshow((y[0, :] - out[0, :]).reshape(s, s).detach().cpu().numpy(), cmap='coolwarm')
+                    # plt.colorbar()
+                    # plt.clim(-0.0005, 0.0005)
+                    # plt.savefig(
+                    #     os.path.join('./results/' + save_name + '/', "case_" + str(id) + "_error.pdf"))
+                    # plt.close()
+                    # # ============ #
+                    # plt.figure()
+                    # plt.axis('off')
+                    # plt.imshow((fx[0, :].unsqueeze(-1)).reshape(s, s).detach().cpu().numpy(), cmap='coolwarm')
+                    # plt.colorbar()
+                    # plt.savefig(
+                    #     os.path.join('./results/' + save_name + '/', "case_" + str(id) + "_input.pdf"))
+                    # plt.close()
+                
+
+
+        rel_err /= ntest
+        print("rel_err:{}".format(rel_err))
+    else:
+        for ep in range(args.epochs):
+            epoch_start_time = time.time()  
+            model.train()
+            train_loss = 0
+
+            for pos, fx, y in train_loader:
+
+                x, fx, y = pos.cuda(), fx.cuda(), y.cuda()  # x:B,N  fx:B,N  y:B,N
+                optimizer.zero_grad()
+                out = model(x, fx=fx.unsqueeze(-1)).squeeze(-1)
+
+                out = y_normalizer.decode(out)
+                y = y_normalizer.decode(y)
+
+                loss = myloss(out, y)
+                loss.backward()
+
+                # print("loss:{}".format(loss.item()/batch_size))
+                if args.max_grad_norm is not None:
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                optimizer.step()
+                train_loss += loss.item()
+                scheduler.step()
+
+            train_loss = train_loss / ntrain
+            print("Epoch {} Train loss : {:.5f}".format(ep, train_loss))
+
+            model.eval()
+            rel_err = 0.0
+            with torch.no_grad():
+                for pos, fx, y in test_loader:
+                    x, fx, y = pos.cuda(), fx.cuda(), y.cuda()
+                    out = model(x, fx=fx.unsqueeze(-1)).squeeze(-1)
+                    out = y_normalizer.decode(out)
+
+                    tl = myloss(out, y).item()
+                    rel_err += tl
+
+            rel_err /= ntest
+            epoch_time = time.time() - epoch_start_time  
+            print("rel_err:{}".format(rel_err),"training time: {:.5f} seconds".format(epoch_time))
+
+            if ep % 100 == 0:
+                if not os.path.exists('./checkpoints'):
+                    os.makedirs('./checkpoints')
+                print('save model')
+                torch.save(model.state_dict(), os.path.join('./checkpoints', save_name + '.pt'))
+
+            with open(loss_log_file, "a") as f:  
+                f.write("{}\t{}\t{}\t{:.5f}\n".format(ep+1, train_loss, rel_err, epoch_time))
+        if not os.path.exists('./checkpoints'):
+            os.makedirs('./checkpoints')
+        print('save model')
+        torch.save(model.state_dict(), os.path.join('./checkpoints', save_name + '.pt'))
+
+
+if __name__ == "__main__":
+    main()
